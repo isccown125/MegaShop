7a8b1278b25c83cdce4b302ed38e7305d0166568
isccown125 <92261514+isccown125@users.noreply.github.com>
2022-11-02 08:37:18 +0100

    Obiekty renderują się na stronie

--- assets/scripts/shop.js
+++ assets/scripts/shop.js
@@ -24,6 +24,16 @@
 
 // alert.error('erorr')
 
+class Component {
+    createElement(element, attr, id, ...className) {
+        if (!element) {
+            throw '1'
+        }
+
+    }
+}
+
+
 class Product {
     constructor() {
         this.id;
@@ -69,6 +79,7 @@ const product = new Product();
 class Shop {
     constructor() {
         this.products = [];
+        this.templateItem = document.getElementsByTagName('template')[0]
     }
 
     addProduct(product) {
@@ -84,36 +95,70 @@ class Shop {
         if (this.products.length > 0) {
             const shop = document.querySelector('#shop');
             const ul = document.createElement('ul');
+
             ul.id = 'products';
             this.products.forEach((el, index) => {
-                console.log(el);
-                const li = document.createElement('li');
-                li.classList.add('products__item');
-                li.textContent = el.title;
-                ul.append(li);
+                if (index <= 30) {
+
+
+                    const divContent = document.createElement('div')
+                    const divImg = document.createElement('div')
+                    const header = document.createElement('header')
+                    const footer = document.createElement('footer');
+                    const title = document.createElement('h3');
+                    const img = document.createElement('img');
+                    const desctiption = document.createElement('p')
+                    const li = document.createElement('li');
+
+                    title.textContent = el.title;
+                    img.src = el.thumbnail;
+                    desctiption.textContent += `${el.price}$ ${el.description}`
+
+
+
+
+                    header.append(title);
+                    divImg.append(img)
+                    footer.append(desctiption)
+
+                    li.classList.add('products-item');
+                    divContent.classList.add('product-item__content');
+                    header.classList.add('product-item__header');
+                    divImg.classList.add('product-item__img');
+                    footer.classList.add('product-item__footer')
+
+
+
+                    divContent.append(header);
+                    divContent.append(divImg)
+                    divContent.append(footer)
+                    li.append(divContent);
+                    ul.append(li);
+
+                }
             })
             shop.append(ul);
         }
     }
 
 }
-// const shop1 = new Shop();
+const shop1 = new Shop();
 
-// (async () => {
-//     const result = await fetch('https://dummyjson.com/products?limit=100&skip=0')
-//     console.log()
-//     const res = await result.json();
-//     res.products.forEach((value, index, array) => {
-//         product.data = value
-//         shop1.addProduct(product.data);
-//     })
-//     console.log(shop1.products)
+(async () => {
+    const result = await fetch('https://dummyjson.com/products?limit=100&skip=0')
+    console.log()
+    const res = await result.json();
+    res.products.forEach((value, index, array) => {
+        product.data = value
+        shop1.addProduct(product.data);
+    })
+    console.log(shop1.products)
 
+    console.log(shop1.templateItem)
 
-
-//     shop1.render();
-//     shop1.renderProducts();
-// })();
+    shop1.render();
+    shop1.renderProducts();
+})();
 
 
 
